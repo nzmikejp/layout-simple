7f12e3e7d5f0e0d8079f30aca2dadad532b6f9d1
Mike Jackson-Potter <nzmikejp@gmail.com>
2020-08-18 16:57:02 +1200

    Here is my simple layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './assets/css/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import View from './View'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      activeView: 'home'
+    }
+  }
+
+  setActiveView = (view) => {
+    this.setState({
+      activeView: view
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+
+        <View viewName="home" activeView={this.state.activeView} className="color1">
+          <div className="header"><i onClick={() => this.setActiveView('nav')} className="fas fa-bars"></i></div>
+          <div className="main">
+            <h3>Home</h3>
+            <p onClick={() => this.setActiveView('about')}>Go to About</p>
+          </div>
+        </View>
+
+        <View viewName="about" activeView={this.state.activeView} className="color2">
+          <div className="header"><i onClick={() => this.setActiveView('nav')} className="fas fa-bars"></i></div>
+          <div className="main">
+            <h3>About</h3>
+            <p onClick={() => this.setActiveView('home')}>Go to Home</p>
+          </div>
+        </View>
+        
+        <View viewName="login" activeView={this.state.activeView} className="color3">
+          <div className="header"><i onClick={() => this.setActiveView('nav')} className="fas fa-bars"></i></div>
+          <div className="main">
+            <h3>Login</h3>
+            <p onClick={() => this.setActiveView('home')}>Go to Home</p>
+          </div>
+        </View>
+        
+        <View viewName="nav" activeView={this.state.activeView} className="color4">
+          <div className="header"><i onClick={() => this.setActiveView('home')} className="fas fa-times"></i></div>
+          <div className="main">
+            <ul className="menu">
+              <li><a onClick={() => this.setActiveView('home')} className="" href="#">Home</a></li>
+              <li><a onClick={() => this.setActiveView('about')} className="" href="#">About</a></li>
+              <li><a onClick={() => this.setActiveView('login')} className="" href="#">Login</a></li>
+            </ul>
+          </div>
+        </View>
+        
+        
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/View.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class View extends Component {
+    
+    render(){
+        var {className, viewName, activeView, children} = this.props
+        var newClassName = 'view ' + className
+
+        return (viewName === activeView) ? (
+
+            <div className={newClassName}>
+                {children}
+            </div>
+
+        ) : null
+    }
+}
+
+export default View
